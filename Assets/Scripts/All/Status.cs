162b4f5bae1f7dbe272000da45b1265c0092aa0b
voltkrugers <80946089+voltkrugers@users.noreply.github.com>
2024-06-20 15:41:01 +0200

    AddStructureProjetScript

---
+++ Assets/Scripts/All/Status.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;using UnityEditor;
+using UnityEngine;
+
+public enum Status
+{
+    NONE,
+    STUN,
+    BURN,
+    CURSE,
+    GROWTH ,
+    FEARSOME,
+    BRASH,
+    REGENERATION,
+    ROBUST,
+    OVERWHELM
+}
